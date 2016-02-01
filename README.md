c5e25d55ebd7a7c37efda2883707b3c501e07b1e
Camby <cambyliverson@gmail.com>
2016-02-01 15:21:43 +0800

    v3.0.0 Docs

---
+++ README.md
@@ -0,0 +1,4 @@
+![SMTB - Semantic UI Bootstrap](http://smtb.cbl-web.com/images/logo-big.png)
+
+# SMTB - Semantic UI Bootstrap
+Please visit [http://smtb.cbl-web.com/](http://smtb.cbl-web.com/) for detail.
---
+++ js/lang.js
@@ -0,0 +1,259 @@
+var langText = {
+    en: {
+        title: 'SMTB | Semantic UI Bootstrap - a Bootstrap theme and component collection inspired by Semantic UI',
+        subject: 'SMTB is a Bootstrap theme and component collection inspired by Semantic UI.',
+        quickOverview: 'Quick Overview',
+        componentNdocs: 'Component',
+        versionDesc: 'Since SMTB designed base on Bootstrap 3, version start from 3.0.0',
+        download: 'Download Now',
+        docsSubject: 'Here we just show the special and new components added to the thmem, for other bootstrap components please find from <a href="http://getbootstrap.com" target="_blank" onclick="ga(\'send\',\'event\',\'extLink\',\'click\',\'getbootstrap\')" style="color:#F3F3F3; border-bottom:1px dotted #F3F3F3;">getbootstrap.com</a>',
+        'Button': 'Button',
+        DefaultButton: 'Default Button',
+        Withicon: 'Button With Icon',
+        Inverted: 'Button Inverted',
+        Circular: 'Circular Button',
+        ButtonGroupDropdown: 'Button Group Dropdown',
+        Dropdown: 'Dropdown',
+        DefaultDropdown: 'Default Dropdown',
+        ShowOnMouseover: 'Show On Mouseover',
+        Submenu: 'Submenu',
+        Tabs: 'Tabs',
+        Alert: 'Alert',
+        Reveal: 'Reveal',
+        Move: 'Move',
+        Rotate: 'Rotate',
+        Fade: 'Fade',
+        DimBlur: 'Dim (Blur overlay)',
+        Circularblock: 'Circular block',
+        Disablehover: 'Disable hover',
+        Label: 'Label',
+        UserLabel: 'User Label',
+        TagLabel: 'Tag Label',
+        Ribbon: 'Ribbon',
+        Attached: 'Attached',
+        Card: 'Card',
+        Divider: 'Divider',
+        HorizontalDivider: 'Horizontal Divider',
+        ButtonDivider: 'Button Divider',
+        Sidebar: 'Sidebar',
+        BodySidebar: 'Body Sidebar',
+        InnerSidebar: 'Inner Sidebar',
+        UsingLess: 'Using Less',
+
+        withIconDesc: 'You can add icon in a button with darker background color. Add class <code>btn-icon-right</code> for icon in the right side, add class <code>btn-icon-left</code> for icon in the left side.',
+        invertedDesc: 'Add class <code>btn-inverted</code> to make outlined button (Also known as ghost button).',
+        invertedDescBold: 'Add class <code>btn-bold</code> to make outline 2px width and bold text.',
+        invertedDescWhite: 'Add class <code>btn-inverted-white</code> to make white outlined button on dark background (default bold).<br>Add class <code>btn-inverted-white-fill</code> to make white outlined button which will be filled on mouseover.',
+        circularButtonDesc: 'Add class <code>btn-circular</code> to make circular button.',
+        buttonDropdownDesc: 'We make the item hightlight follow the class of the toggle button in button group dropdown.<br>The code is same as in bootstrap.',
+        dropdownHoverDesc: 'Add class <code>dropdown-hover</code> to show dropdown menu on mouseover.',
+        DefaultTabs: 'Default Tabs',
+        MaterialTabs: 'Material Tabs',
+        materialTabsDesc: 'Add class <code>material-tabs</code> to change tabs to material style.',
+        DefaultAlert: 'Default Alert',
+        darkAlertDesc: 'Add class <code>alert-dark</code> to make dark color alert box.',
+        DefaultLabel: 'Default Label',
+        revealDesc: 'Reveal is a in out transitional effect of two block inside a container.',
+        revealLeftDesc: 'Apply move left effect by the class <code>reveal-move</code>',
+        revealRightDesc: 'Apply move right effect by the class <code>reveal-move-right</code>',
+        revealUpDesc: 'Apply move up effect by the class <code>reveal-move-up</code>',
+        revealDownDesc: 'Apply move down effect by the class <code>reveal-move-down</code>',
+        revealNoLeaveDesc: 'Add class <code>reveal-no-leave</code> to keep the default shown block in position.',
+        rotateLeftDesc: 'Apply rotate left effect by the class <code>reveal-rotate</code>',
+        rotateRightDesc: 'Apply rotate right effect by the class <code>reveal-rotate-right</code>',
+        revealFadeDesc: 'Apply fade effect by the class <code>reveal-fade</code>',
+        revealDimDesc: 'Apply dim effect (blur overlay) by the class <code>reveal-dim</code>',
+        revealDimWhiteDesc: 'Add <code>reveal-dim-white</code> class to change the background to white for dim effect.',
+        revealCircularDesc: 'Make circular reveal block by the class <code>reveal-circular</code>.',
+        DisableEffectOnMouseover: 'Disable Effect On Mouseover',
+        DisableHoverDesc: 'Disable effect on mouseover by the class <code>reveal-no-hover</code>, this is useful when you want to control which block to be shown on the screen by javascript.',
+        ToggleActive: 'ToggleActive',
+        userLabelDesc: 'Class <code>label-image</code> make an image inside a label fit attached on the left.',
+        labelDetailDesc: 'Add <code>label-detail</code> class to an element inside a label for description (width darker background).',
+        labelIconDesc: 'Add <code>label-icon</code> class to mark a icon. (suitable for set up a dismiss button as shown in example)',
+        tagLabelDesc: 'Label styled as a tag when class <code>label-tag</code> is added.',
+        labelRibbonDesc: 'Label styled as a ribbon when class <code>label-ribbon-left</code> or <code>label-ribbon-right</code> is added. (It should use with a .well block)',
+        labelRibbonOnCardDesc: 'Ribbon can add on a <a href="#card">card</a>.',
+        labelAttachedTopDesc: 'Add class <code>label-attached-top</code> to attach a label on the top.',
+        labelAttachedBottomDesc: 'Add class <code>label-attached-bottom</code> to attach a label on the bottom.',
+        labelAttachedCornerDesc: 'Add class <code>label-top-left</code>, <code>label-top-right</code>, <code>label-bottom-left</code>, <code>label-bottom-right</code> to attach a label on the corner. (Please add margin by yourself to the top or bottom to prevent too close of the content with the label.)',
+        cardDesc: 'A card displays site content in a manner similar to a playing card.',
+        CardLink: 'Card Link',
+        cardLinkDesc: 'Add class <code>card-link</code> to make a card act as a link element. (Will be raised up on mouseover)',
+        CardReveal: 'Card with Reveal Effect',
+        cardRevealDesc: 'You can use reveal effect on a card.',
+        RevealRotate: 'RevealRotate',
+        RevealDim: 'Reveal Dim',
+        RevealDimWhite: 'Reveal Dim (white)',
+        dividerDesc: 'A divider divide two block of content, add class <code>bs-divider</code> to turn an element to a horizontal divider.',
+        Or: 'Or',
+        titleDividerDesc: 'A divider can act as a heading.',
+        dividerAsHeading: 'Divider can act as a heading',
+        buttonDividerDesc: 'Add class <code>btn-divided</code> to a button group and add <code>&lt;i data-text="or">&lt;/i></code> inside to act as the divider, you can define custom text by <code>data-text</code>.',
+        sidebarDesc: '<p>SMTB didn\'t add js code to the package, you need to control the sidebar by add/remove class.</p> \
+                        <p>There are two type of sidebar available:<br>- <a href="../component#sidebar-body">Body Sidebar</a><br>- <a href="../component#sidebar-inner">Inner Sidebar</a></p> \
+                        <p>Add below code to allow dismiss on the click and touch event on sidebar overlay.</p>',
+        bodySidebarDesc: '<p>Add <code>sidebar-right</code> to show the sidebar in the right side, otherwise, remove it (same in inner sidebar).</p> \
+                        <p>Use <code>dropdown</code> to make hidden submenu, don\'t use it if you want to show it anytime (same in inner sidebar).</p> \
+                        <p>Add an empty <code>a</code> element inside the <code>.sidebar-overlay</code> to show a cross icon near the sidebar.</p> \
+                        <p>Add class <code>active</code> to <code>.sidebar</code> to active the sidebar</p> \
+                        <p>Add class <code>sidebar-active</code> to <code>body</code> if you want to disable scrolling.</p> \
+                        <p>A body sidebar should in the first level under the <code>body</code> tag.</p>',
+        innerSidebarDesc: '<p>Add <code>sidebar-right</code> to show the sidebar in the right side, otherwise, remove it (same in body sidebar).</p> \
+                        <p>Use <code>dropdown</code> to make hidden submenu, don\'t use it if you want to show it anytime (same in body sidebar).</p> \
+                        <p>Add an empty <code>a</code> element inside the <code>.sidebar-overlay</code> to show a cross icon near the sidebar.</p> \
+                        <p>Add class <code>active</code> to <code>.sidebar-container</code> to active the sidebar. (<i class="fa fa-exclamation-triangle"></i> Different in body sidebar)</p> \
+                        <p>Add class <code>sidebar-push</code> to <code>.sidebar-container</code> to use push effect (inner sidebar only).</p>',
+        InnerSidebarLeft: 'Inner Sidebar in the left',
+        InnerSidebarRight: 'Inner Sidebar in the right with push effect',
+        usingLessDesc: 'SMTB write with LESS CSS, you can easily cutomize by changing the color variable.<br>There are only ten variable defined in mixin.less:',
+        BrandColor: 'Brand Color',
+        AlertColor: 'Alert Color',
+        alertColorDesc: '[1] Background-color, [2] border-color, [3] font color',
+        finalDesc: 'SMTB designed base on bootstrap 3.3.6, however, it didn\'t merged with the bootstrap soruce, so you can use most version of bootstrap 3 as a base and overide the style by adding SMTB css after bootstrap source css. (i.e. You can use a customized bootstrap package as a base)',
+    },
+    zh: {
+        title: 'SMTB | Semantic UI Bootstrap - 一個模仿 Semantic UI 的 Bootstrap 主題',
+        subject: 'SMTB 是一個基於 Bootstrap 的主題及元件合集<br>靈感來自 Semantic UI',
+        quickOverview: '快速概覽',
+        componentNdocs: '元件介紹及文檔',
+        download: '立即下載',
+        versionDesc: '由於 SMTB 是基於 Bootstrap 3 開發，因此版本號以 3.0.0 為起始數',
+        docsSubject: '這裡我們只列出一些經過特別修改和SMTB獨有新加的元件，關於其他Bootstrap元件的說明請查看 <a href="http://getbootstrap.com" target="_blank" onclick="ga(\'send\',\'event\',\'extLink\',\'click\',\'getbootstrap\')" style="color:#F3F3F3; border-bottom:1px dotted #F3F3F3;">getbootstrap.com</a>。',
+        'Button': '按鈕',
+        DefaultButton: '原始按鈕',
+        Withicon: '含圖示按鈕',
+        Inverted: '框線按鈕',
+        Circular: '圓形按鈕',
+        ButtonGroupDropdown: '組合按鈕和下拉菜單',
+        Dropdown: '下拉菜單',
+        DefaultDropdown: '原始下拉菜單',
+        ShowOnMouseover: '鼠標懸浮時顯示',
+        Submenu: '子下拉菜單',
+        Tabs: '標籤頁',
+        Alert: '警告框',
+        Reveal: '動畫切換特效',
+        Move: '移動',
+        Rotate: '旋轉',
+        Fade: '漸變',
+        DimBlur: '變暗 (模糊)',
+        Circularblock: '圓形區塊',
+        Disablehover: '停用懸浮時變換',
+        Label: '標籤',
+        UserLabel: '用戶標籤',
+        TagLabel: 'Tag標籤',
+        Ribbon: '緞帶',
+        Attached: '黏附',
+        Card: '卡片',
+        Divider: '分隔線',
+        HorizontalDivider: '水平分隔線',
+        ButtonDivider: '按鈕分隔線',
+        Sidebar: '側邊欄',
+        BodySidebar: '主體側邊欄',
+        InnerSidebar: '內文側邊欄',
+        UsingLess: '使用LESS',
+
+        withIconDesc: '你可以將圖示加到按鈕中並且以較深的背景顏色突顯之. 在圖示中加上<code>btn-icon-right</code>類別以將其顯示在右邊, 加上<code>btn-icon-left</code>類別則會顯示在左邊。',
+        invertedDesc: '在按鈕增加類別<code>btn-inverted</code>會將其變成框線按鈕（別名幽靈按鈕）。',
+        invertedDescBold: '在框線按鈕增加類別<code>btn-bold</code>會將框線和文字變粗。',
+        invertedDescWhite: '在按鈕增加類別<code>btn-inverted-white</code>會將按鈕變成白色框線按鈕，適用於較黑的背景上（白色框線按鈕預設已是粗線狀態）。<br>改用<code>btn-inverted-white-fill</code>會將按鈕變成懸浮時填滿顏色的白色框線按鈕。',
+        circularButtonDesc: '在按鈕增加類別<code>btn-circular</code>可將按鈕變成圓形。',
+        buttonDropdownDesc: '我們將配合按鈕顯示的下拉菜單的項目高亮顏色改為跟隨按鈕顏色改變，而非永遠顯示淺灰色。',
+        dropdownHoverDesc: '在下拉菜單新增類別<code>dropdown-hover</code>可令菜單在鼠標懸浮時即自動顯示。',
+        DefaultTabs: '原始標籤頁',
+        MaterialTabs: 'Material樣式標籤頁',
+        materialTabsDesc: '在標籤頁增加類別<code>material-tabs</code>可使其變成Material樣式。',
+        DefaultAlert: '原始警告框',
+        darkAlertDesc: '在警告框增加類別<code>alert-dark</code>會將警告框變為深色背景色。',
+        DefaultLabel: '原始標籤',
+        revealDesc: '變換特效是指兩個區塊在同一容器內切換顯示的動畫特效。',
+        revealLeftDesc: '使用<code>reveal-move</code>類別制作左移切換動畫。',
+        revealRightDesc: '使用<code>reveal-move-right</code>類別制作右移切換動畫。',
+        revealUpDesc: '使用<code>reveal-move-up</code>類別制作上移切換動畫。',
+        revealDownDesc: '使用<code>reveal-move-down</code>類別制作下移切換動畫。',
+        revealNoLeaveDesc: '增加<code>reveal-no-leave</code>類別以固定原區塊不切出。',
+        rotateLeftDesc: '使用<code>reveal-rotate</code>類別制作向左旋轉切換動畫。',
+        rotateRightDesc: '使用<code>reveal-rotate-right</code>類別制作向右旋轉切換動畫。',
+        revealFadeDesc: '使用<code>reveal-fade</code>類別制作漸變切換動畫。',
+        revealDimDesc: '使用<code>reveal-dim</code>類別制作變暗模糊遮罩。',
+        revealDimWhiteDesc: '增加<code>reveal-dim-white</code>以將遮罩轉為白色背景。',
+        revealCircularDesc: '增加<code>reveal-circular</code>以將切換區塊改為圓形。',
+        DisableEffectOnMouseover: '禁止鼠標懸浮時的動畫切換',
+        DisableHoverDesc: '增加<code>reveal-no-hover</code>以禁止區塊在鼠標懸浮時的切換，這適合用於一些需要用 javascript 於指定條件下才切換的狀況下。',
+        ToggleActive: '切換顯示區塊',
+        userLabelDesc: '在標籤增加類別<code>label-image</code>可令標籤中的圖片剛好貼緊在左邊。',
+        labelDetailDesc: '使用<code>label-detail</code>以較深的背景顏色標示額外資訊。',
+        labelIconDesc: '使用<code>label-icon</code>以在標籤中增加一個圖示。（適合用作增加一個關閉按鈕）',
+        tagLabelDesc: '在標籤增加類別<code>label-tag</code>以將其變為Tag樣式',
+        labelRibbonDesc: '在標籤增加類別<code>label-ribbon-left</code>或<code>label-ribbon-right</code>以將其變成緞帶樣式。（需要搭配.well區塊使用）',
+        labelRibbonOnCardDesc: '緞帶樣式的標籤也可以搭配<a href="#card">卡片</a>使用。',
+        labelAttachedTopDesc: '在標籤增加類別<code>label-attached-top</code>以將其黏附在上方。',
+        labelAttachedBottomDesc: '在標籤增加類別<code>label-attached-bottom</code>以將其黏附在下方。',
+        labelAttachedCornerDesc: '在標籤增加類別<code>label-top-left</code>, <code>label-top-right</code>, <code>label-bottom-left</code>或<code>label-bottom-right</code>以將其黏附在四個角落。（你需要自行在容器中加入適當的間距以避免標籤遮蔽了內容）',
+        cardDesc: '卡片以類似遊戲卡的樣式展示不同的網站內容。',
+        CardLink: '卡片連結',
+        cardLinkDesc: '在卡片增加類別<code>card-link</code>以令其在鼠標懸浮時浮起。',
+        CardReveal: '附帶切換特效的卡片',
+        cardRevealDesc: '你可以在卡片中搭配動畫切換特效。',
+        RevealRotate: '旋轉切換',
+        RevealDim: '變暗遮罩',
+        RevealDimWhite: '變暗遮罩（白色）',
+        dividerDesc: '分隔線用於分隔兩個不同區塊的內容，在物件增加<code>bs-divider</code>以將其變成一條分隔線',
+        Or: '或',
+        titleDividerDesc: '分隔線也可以作為標題的形式存在。',
+        dividerAsHeading: '分隔線可以是一個標題',
+        buttonDividerDesc: '在<code>btn-group</code>增加<code>btn-divided</code>類別並且在當中的按鈕與按鈕之間加上<code>&lt;i data-text="or">&lt;/i></code>以制作按鈕分隔線，你可以用<code>data-text</code>自定義要顯示的文字。',
+        sidebarDesc: '<p>SMTB是一個純CSS合集，沒有加上javascript的控制，因此你需要自行在側邊欄上用js增加/移除類別以切換顯示狀態。</p> \
+                        <p>SMTB提供兩種側邊欄<br>- <a href="../component#sidebar-body">主體側邊欄</a><br>- <a href="../component#sidebar-inner">內文側邊欄</a></p> \
+                        <p>你需要在Document Ready時增加以下的js程式碼以令用戶在點按遮罩時關閉側邊欄。</p>',
+        bodySidebarDesc: '<p>在側邊欄增加類別<code>sidebar-right</code>以將其改到右邊彈出，否則請移除<code>sidebar-right</code>。（內頁側邊欄也一樣）</p> \
+                        <p>利用<code>dropdown</code>以制作下拉式菜單，如要固定顯示所有菜單內容則不要用<code>dropdown</code>。（內頁側邊欄也一樣）</p> \
+                        <p>在側邊欄遮罩<code>.sidebar-overlay</code>中增加一個空的<code>a</code>元素可顯示出一個交叉圖示，以讓用知道按那裡可以關閉側邊欄。</p> \
+                        <p>以js在側邊欄增加<code>active</code>類別便可將側邊欄喚出。</p> \
+                        <p>如要禁止背景內容滾動，可以在<code>body</code>增加<code>sidebar-active</code>類別。</p> \
+                        <p>主體側邊欄必需緊隨在<code>body</code>下一層級。</p>',
+        innerSidebarDesc: '<p>在側邊欄增加類別<code>sidebar-right</code>以將其改到右邊彈出，否則請移除<code>sidebar-right</code>。（主體側邊欄也一樣）</p> \
+                        <p>利用<code>dropdown</code>以制作下拉式菜單，如要固定顯示所有菜單內容則不要用<code>dropdown</code>。（主體側邊欄也一樣）</p> \
+                        <p>在側邊欄遮罩<code>.sidebar-overlay</code>中增加一個空的<code>a</code>元素可顯示出一個交叉圖示，以讓用戶知道按那裡可以關閉側邊欄。</p> \
+                        <p>以js在側邊欄容器<code>.sidebar-container</code>增加<code>active</code>類別便可將側邊欄喚出。（<i class="fa fa-exclamation-triangle"></i> 有別於主體側邊欄）</p> \
+                        <p>在側邊欄容器<code>.sidebar-container</code>增加類別<code>sidebar-push</code>可令內容在側邊欄彈出時做出推動的效果。（只適用於內頁側邊欄）</p>',
+        InnerSidebarLeft: '左邊內頁側邊欄',
+        InnerSidebarRight: '右邊內頁側邊欄（含推出效果）',
+        usingLessDesc: 'SMTB 是以 LESS CSS 編寫，你可以輕鬆地更改定義在<code>mixin.less</code>當中的顏色變數以建立你需要的色彩主題：',
+        BrandColor: '主題顏色',
+        AlertColor: '警告框顏色',
+        alertColorDesc: '[1] 背景顏色, [2] 框線顏色, [3] 文字顏色',
+        finalDesc: 'SMTB 基於 Bootstrap 3.3.6 編寫，然而它並沒有和 Bootstrap 源碼結合在一起，因此你可以使用任何一個 Bootstrap 3 的子版本作為根底，換言之，你可以使用 Bootstrap Customize 服務建立自己適用的 Bootstrap 套件作為基底然後才在後面加上 SMTB。',
+    }
+};
+var lang = window.location.hash == '#!zh' ? 'zh' : (localStorage.smtbLang ? localStorage.smtbLang : getLang());
+if(lang=='zh') { window.location.hash = '#!zh'; }
+else { window.location.hash = ''; }
+
+function getLang() {
+    var type=navigator.appName;
+    if (type=="Netscape"){
+        var lang = navigator.language;
+    } else{
+        var lang = navigator.userLanguage;
+    }
+    var lang = lang.substr(0,2);
+    if (lang == "zh"){
+        return 'zh';
+    } else {
+        return 'en';
+    }
+};
+
+var ngApp = angular.module("app", []); 
+ngApp.run(function($rootScope) {
+    $rootScope.language = lang;
+    $rootScope.lang = langText[lang];
+    $rootScope.changeLang = function(l) {
+        if(l=='zh') { window.location.hash = '#!zh'; }
+        else { window.location.hash = ''; }
+        localStorage.smtbLang = l;
+        $rootScope.language = l;
+        $rootScope.lang = langText[l];
+    };
+});
